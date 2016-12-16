62ac3c40b95e550fc94e8110d1641d339bf534bc
fangjn <fangjinniscut@gmail.com>
2016-12-16 23:38:55 +0800

    remove the rebundant end in post_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
-  end
 
   def create
     @group = Group.find(params[:group_id])
